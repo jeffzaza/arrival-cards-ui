23f41b9fa8671c81a98ab2163bf3b23dce647fc5
jeff <jeff@localhost.localdomain>
2025-07-23 21:33:56 +0800

    Try to import Amplify UI theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, AmplifyAuthenticatorModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
