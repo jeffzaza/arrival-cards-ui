9789a4c2a28bfda6e4e0a8957e3732f623d6da2c
jeff <jeff@localhost.localdomain>
2025-07-23 17:36:39 +0800

    load amplify libraries

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
