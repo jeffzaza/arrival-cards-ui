c4b5a6cad255570c3cf0864fbabdf50647c3eecb
jeff <jeff@localhost.localdomain>
2025-07-23 17:52:41 +0800

    Add create and manage pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreateComponent } from './pages/create/create';
+import { ManageComponent } from './pages/manage/manage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'manage', component: ManageComponent },
+  { path: '', redirectTo: 'manage', pathMatch: 'full' }
+];
+
